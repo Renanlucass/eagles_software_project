dde9889beadc23518df00a9b4b231e70898a094c
Renanlucass <renanlucas0014@gmail.com>
2023-06-15 13:15:47 -0300

    Atualizando

---
+++ js/main.js
@@ -0,0 +1,15 @@
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+        .then(function (registration) {
+            console.log('Service Worker registrado com sucesso:', registration);
+        })
+        .catch(function (error) {
+            console.log('Falha ao registrar o Service Worker:', error);
+        });
+
+    window.addEventListener('beforeinstallprompt', function (event) {
+        event.preventDefault();
+        var deferredPrompt = event;
+
+    });
+}
